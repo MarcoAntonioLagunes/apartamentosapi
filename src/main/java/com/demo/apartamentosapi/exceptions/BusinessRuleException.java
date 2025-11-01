52cca04d0465ec4c197100bd75b584e49279104d
Marco Lagunes <202260487@ucc.mx>
2025-11-01 12:31:36 -0600

    Revert "Commit 0"
    This reverts commit fae48d033e812494cd9b417970a0ca638e2f9c14.

--- src/main/java/com/demo/apartamentosapi/exceptions/BusinessRuleException.java
+++
@@ -1,7 +0,0 @@
-package com.demo.apartamentosapi.exceptions;
-
-public class BusinessRuleException extends RuntimeException {
-    public BusinessRuleException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/demo/apartamentosapi/exceptions/ReferentialIntegrityException.java
+++
@@ -1,7 +0,0 @@
-package com.demo.apartamentosapi.exceptions;
-
-public class ReferentialIntegrityException extends RuntimeException {
-    public ReferentialIntegrityException(String message) {
-        super(message);
-    }
-}
